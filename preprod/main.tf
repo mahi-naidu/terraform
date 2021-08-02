e5ed59e4855d6aa88d517b8ae6411520041ef463
second git topic <mahinaidu1432s@gmail.com>
2021-08-02 13:08:23 +0530

    modifies the env name

--- preprod/main.tf
+++ preprod/main.tf
@@ -2,7 +2,7 @@ module "stage" {
       source ="../vpc/"
       region = "ap-northeast-1"
       cidr = "10.3.0.0/16"
-      envname = "tera-dev"
+      envname = "tera-preprod"
       pubsubnets = ["10.3.0.0/24","10.3.1.0/24","10.3.2.0/24"]
       privsubnets = ["10.3.3.0/24","10.3.4.0/24","10.3.5.0/24"]
      datasubnets = ["10.3.6.0/24","10.3.7.0/24","10.3.8.0/24"]
--- prod/main.tf
+++ prod/main.tf
@@ -2,7 +2,7 @@ module "stage" {
       source ="../vpc/"
       region = "ap-northeast-1"
       cidr = "10.4.0.0/16"
-      envname = "tera-dev"
+      envname = "tera-prod"
       pubsubnets = ["10.4.0.0/24","10.4.1.0/24","10.4.2.0/24"]
       privsubnets = ["10.4.3.0/24","10.4.4.0/24","10.4.5.0/24"]
      datasubnets = ["10.4.6.0/24","10.4.7.0/24","10.4.8.0/24"]
